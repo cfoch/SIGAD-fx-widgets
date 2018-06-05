b48a95f60840187ebb789af679755117cb790ff7
Fabian Orccon <cfoch.fabian@gmail.com>
2018-06-05 16:35:28 -0500

    scripts: Agregar script de instalación de schemas

---
+++ scripts/install_json_schemas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p src/main/resources/com/sigad/sigad/fx/widgets/map/
+cp common/json-schemas/solution.json.schema src/main/resources/com/sigad/sigad/fx/widgets/map/solution.json.schema
