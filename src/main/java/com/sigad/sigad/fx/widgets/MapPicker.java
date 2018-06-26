08bf50eda44b085400e71de7bf1c70cabe845078
Fabian Orccon <cfoch.fabian@gmail.com>
2018-06-25 21:17:34 -0500

    VRPMapView: Notificar cuando un marker es agregado

--- src/main/java/com/sigad/sigad/fx/widgets/MapPicker.java
+++ src/main/java/com/sigad/sigad/fx/widgets/MapPicker.java
@@ -6,7 +6,11 @@
 package com.sigad.sigad.fx.widgets;
 
 import com.sun.javafx.webkit.WebConsoleListener;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.net.URL;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -17,6 +21,22 @@ import netscape.javascript.JSObject;
  * @author cfoch
  */
 public class MapPicker extends Region {
+    public static class Bridge {
+        MapPicker mapPicker;
+
+        public Bridge(MapPicker mapPicker) {
+            this.mapPicker = mapPicker;
+        }
+        public void markerAddedCb() {
+            Double [] latlng = mapPicker.getLatLng();
+            System.out.println("marker-added");
+            // FIXME
+            // Old value should be different than null.
+            mapPicker.getMarkerAddedProp()
+                    .firePropertyChange("marker-added", null, latlng);
+        }
+    }
+
     private static final String DEFAULT_URL = "/html/map-picker.html";
     private static final String CONSOLE_LOG_TMPL = "Console: [%s:%d] %s";
 
@@ -30,6 +50,7 @@ public class MapPicker extends Region {
     private WebView browser;
     private WebEngine webEngine;
     private String solution;
+    private PropertyChangeSupport markerAddedProp;
 
     public MapPicker() {
         URL url;
@@ -39,6 +60,17 @@ public class MapPicker extends Region {
         webEngine = browser.getEngine();
         webEngine.load(url.toString());
         webEngine.setJavaScriptEnabled(true);
+
+        markerAddedProp = new PropertyChangeSupport(this);
+
+        webEngine.getLoadWorker()
+                .stateProperty().addListener(
+                        (ObservableValue<? extends Worker.State> observable,
+                                Worker.State oldValue,
+                                Worker.State newValue) -> {
+            JSObject window = (JSObject) webEngine.executeScript("window");
+            window.setMember("bridge", new Bridge(this));
+        });
 
         WebConsoleListener.setDefaultListener(new WebConsoleListener(){
             @Override
@@ -75,6 +107,10 @@ public class MapPicker extends Region {
                 lat, lng));
     }
 
+    public void addMarkerAddedPropListener(PropertyChangeListener listener) {
+        getMarkerAddedProp().addPropertyChangeListener(listener);
+    }
+
     /**
      * @return the webEngine
      */
@@ -87,5 +123,12 @@ public class MapPicker extends Region {
      */
     public WebView getWebView() {
         return browser;
+    }
+
+    /**
+     * @return the markerAddedProp
+     */
+    public PropertyChangeSupport getMarkerAddedProp() {
+        return markerAddedProp;
     }
 }
